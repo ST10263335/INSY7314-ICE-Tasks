435ea56ed82e51d52f36dd1a2e3f39980c52732d
Alicia <st10263335@vcconnect.edu.za>
2025-09-25 18:38:24 +0200

    Add Lab Guide ICE Task 2

---
+++ Backend/Server.mjs
@@ -0,0 +1,34 @@
+import https from "https";
+import fs from "fs";
+import posts from "./routes/post.mjs"
+import users from "./routes/users.mjs"
+import express from  "express";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+
+const options ={
+    key: fs.readFileSync('keys/privatekey.pem'),
+    cert: fs.readFileSync('keys/certificate.pem')
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use((reg,res,next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    next();
+})
+
+app.use("/posts", posts);
+app.route("/posts", posts);
+app.use("/user", users);
+//app.route("/user", users)
+
+let server = https.createServer(options,app)
+console.log(PORT)
+server.listen(PORT);
